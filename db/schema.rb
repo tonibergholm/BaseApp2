4d1aa77a7ec195ff584601bae93ad2421688b9e1
Danny McClelland <mail@dannyweb.net>
2010-07-30 17:42:22 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100730095943) do
+ActiveRecord::Schema.define(:version => 20100730163615) do
 
   create_table "announcements", :force => true do |t|
     t.text     "message"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20100730095943) do
     t.datetime "deleted_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
